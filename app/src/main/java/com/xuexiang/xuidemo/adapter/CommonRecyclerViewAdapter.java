f212163b9ee6dff33e4666ae4b6f7e21fab1ae24
xuexiangjys <xuexiangjys@163.com>
2019-01-03 17:01:09 +0800

    增加SnapHelper使用演示

---
+++ app/src/main/java/com/xuexiang/xuidemo/adapter/CommonRecyclerViewAdapter.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright (C) 2019 xuexiangjys(xuexiangjys@163.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xuexiang.xuidemo.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.TextView;
+
+import com.xuexiang.xuidemo.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 通用的 RecyclerView Adapter。
+ *
+ * @author xuexiang
+ * @since 2019/1/3 下午4:54
+ */
+public class CommonRecyclerViewAdapter extends RecyclerView.Adapter<CommonRecyclerViewAdapter.ViewHolder> {
+
+    private List<Data> mItems;
+    private AdapterView.OnItemClickListener mOnItemClickListener;
+
+    public CommonRecyclerViewAdapter() {
+        mItems = new ArrayList<>();
+    }
+
+    public static List<Data> generateDatas(int count) {
+        ArrayList<Data> mDatas = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            mDatas.add(new Data(String.valueOf(i)));
+        }
+        return mDatas;
+    }
+
+    public void addItem(int position) {
+        if (position > mItems.size()) return;
+
+        mItems.add(position, new Data(String.valueOf(position)));
+        notifyItemInserted(position);
+    }
+
+    public void removeItem(int position) {
+        if (position >= mItems.size()) return;
+
+        mItems.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View root = inflater.inflate(R.layout.adapter_item_common_recycler_view, viewGroup, false);
+        return new ViewHolder(root, this);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        Data data = mItems.get(i);
+        viewHolder.setText(data.text);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    public void setItemCount(int count) {
+        mItems.clear();
+        mItems.addAll(generateDatas(count));
+
+        notifyDataSetChanged();
+    }
+
+    public void setOnItemClickListener(AdapterView.OnItemClickListener onItemClickListener) {
+        mOnItemClickListener = onItemClickListener;
+    }
+
+    private void onItemHolderClick(RecyclerView.ViewHolder itemHolder) {
+        if (mOnItemClickListener != null) {
+            mOnItemClickListener.onItemClick(null, itemHolder.itemView,
+                    itemHolder.getAdapterPosition(), itemHolder.getItemId());
+        }
+    }
+
+    public static class Data {
+        public String text;
+
+        public Data(String text) {
+            this.text = text;
+        }
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private TextView mTextView;
+        private CommonRecyclerViewAdapter mAdapter;
+
+        public ViewHolder(View itemView, CommonRecyclerViewAdapter adapter) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+
+            mAdapter = adapter;
+
+            mTextView = (TextView) itemView.findViewById(R.id.textView);
+        }
+
+        public void setText(String text) {
+            mTextView.setText(text);
+        }
+
+
+        @Override
+        public void onClick(View v) {
+            mAdapter.onItemHolderClick(this);
+        }
+    }
+}
---
+++ app/src/main/java/com/xuexiang/xuidemo/fragment/expand/SnapHelperFragment.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2019 xuexiangjys(xuexiangjys@163.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xuexiang.xuidemo.fragment.expand;
+
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PagerSnapHelper;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SnapHelper;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.xuexiang.xpage.annotation.Page;
+import com.xuexiang.xui.widget.actionbar.TitleBar;
+import com.xuexiang.xui.widget.dialog.bottomsheet.BottomSheet;
+import com.xuexiang.xuidemo.R;
+import com.xuexiang.xuidemo.adapter.CommonRecyclerViewAdapter;
+import com.xuexiang.xuidemo.base.BaseFragment;
+
+import butterknife.BindView;
+
+/**
+ * 使用 {@link SnapHelper} 实现 {@link RecyclerView} 按页滚动。
+ *
+ * @author xuexiang
+ * @since 2019/1/3 下午4:49
+ */
+@Page(name = "SnapHelper使用", extra = R.drawable.ic_expand_snap_helper)
+public class SnapHelperFragment extends BaseFragment {
+
+    @BindView(R.id.pagerWrap)
+    ViewGroup mPagerWrap;
+
+    RecyclerView mRecyclerView;
+    LinearLayoutManager mPagerLayoutManager;
+    CommonRecyclerViewAdapter mRecyclerViewAdapter;
+    SnapHelper mSnapHelper;
+
+    @Override
+    protected TitleBar initTitle() {
+        TitleBar titleBar = super.initTitle();
+        titleBar.addAction(new TitleBar.ImageAction(R.drawable.icon_topbar_overflow) {
+            @Override
+            public void performAction(View view) {
+                showBottomSheetList();
+            }
+        });
+        return titleBar;
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_pagerlayoutmanager;
+    }
+
+    @Override
+    protected void initViews() {
+        mRecyclerView = new RecyclerView(getContext());
+        mPagerLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        mRecyclerView.setLayoutManager(mPagerLayoutManager);
+        mRecyclerViewAdapter = new CommonRecyclerViewAdapter();
+        mRecyclerViewAdapter.setItemCount(10);
+        mRecyclerView.setAdapter(mRecyclerViewAdapter);
+        mPagerWrap.addView(mRecyclerView);
+        // PagerSnapHelper每次只能滚动一个item;用LinearSnapHelper则可以一次滚动多个，并最终保证定位
+        // mSnapHelper = new LinearSnapHelper();
+        mSnapHelper = new PagerSnapHelper();
+        mSnapHelper.attachToRecyclerView(mRecyclerView);
+    }
+
+    private void showBottomSheetList() {
+        new BottomSheet.BottomListSheetBuilder(getActivity())
+                .addItem("水平方向")
+                .addItem("垂直方向")
+                .setOnSheetItemClickListener(new BottomSheet.BottomListSheetBuilder.OnSheetItemClickListener() {
+                    @Override
+                    public void onClick(BottomSheet dialog, View itemView, int position, String tag) {
+                        dialog.dismiss();
+                        switch (position) {
+                            case 0:
+                                mPagerLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+                                break;
+                            case 1:
+                                mPagerLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                })
+                .build()
+                .show();
+    }
+
+
+}
