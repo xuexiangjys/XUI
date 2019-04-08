aabbfa4c59667ead778504afbdaad90e00cce6d5
xuexiangjys <xuexiangjys@163.com>
2019-04-08 17:56:26 +0800

    增加使用Broccoli占位的基础适配器

--- app/src/main/java/com/xuexiang/xuidemo/adapter/NewsListAdapter.java
+++ app/src/main/java/com/xuexiang/xuidemo/adapter/NewsListAdapter.java
@@ -1,19 +1,13 @@
 package com.xuexiang.xuidemo.adapter;
 
-import android.view.View;
-
-import com.scwang.smartrefresh.layout.adapter.SmartRecyclerAdapter;
 import com.scwang.smartrefresh.layout.adapter.SmartViewHolder;
 import com.xuexiang.xui.widget.imageview.RadiusImageView;
 import com.xuexiang.xui.widget.imageview.preview.MediaLoader;
 import com.xuexiang.xuidemo.DemoDataProvider;
 import com.xuexiang.xuidemo.R;
+import com.xuexiang.xuidemo.adapter.base.BroccoliRecyclerAdapter;
 import com.xuexiang.xuidemo.adapter.entity.NewInfo;
 import com.xuexiang.xuidemo.utils.PlaceholderHelper;
-
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 import me.samlss.broccoli.Broccoli;
 
@@ -21,104 +15,73 @@ import me.samlss.broccoli.Broccoli;
  * @author xuexiang
  * @since 2019/4/7 下午12:06
  */
-public class NewsListAdapter extends SmartRecyclerAdapter<NewInfo> {
-
-
-    /**
-     * 是否已经加载成功
-     */
-    private boolean mHasLoad = false;
-
+public class NewsListAdapter extends BroccoliRecyclerAdapter<NewInfo> {
     /**
      * 是否是加载占位
      */
     private boolean mIsAnim;
-
-
-    private Map<View, Broccoli> mBroccoliMap = new HashMap<>();
-
 
     public NewsListAdapter(boolean isAnim) {
         super(DemoDataProvider.getEmptyNewInfo(), R.layout.adapter_news_list_item);
         mIsAnim = isAnim;
     }
 
-
     /**
-     * 绑定布局控件
+     * 绑定控件
      *
      * @param holder
      * @param model
      * @param position
      */
     @Override
-    protected void onBindViewHolder(SmartViewHolder holder, NewInfo model, int position) {
-        Broccoli broccoli = mBroccoliMap.get(holder.itemView);
-        if (broccoli == null) {
-            broccoli = new Broccoli();
-            mBroccoliMap.put(holder.itemView, broccoli);
-        }
-        if (mHasLoad) {
-            broccoli.removeAllPlaceholders();
+    protected void onBindData(SmartViewHolder holder, NewInfo model, int position) {
+        holder.text(R.id.tv_user_name, model.getUserName());
+        holder.text(R.id.tv_tag, model.getTag());
+        holder.text(R.id.tv_title, model.getTitle());
+        holder.text(R.id.tv_summary, model.getSummary());
+        holder.text(R.id.tv_praise, model.getPraise() == 0 ? "点赞" : String.valueOf(model.getPraise()));
+        holder.text(R.id.tv_comment, model.getComment() == 0 ? "评论" : String.valueOf(model.getComment()));
+        holder.text(R.id.tv_read, "阅读量 " + model.getRead());
 
-            holder.text(R.id.tv_user_name, model.getUserName());
-            holder.text(R.id.tv_tag, model.getTag());
-            holder.text(R.id.tv_title, model.getTitle());
-            holder.text(R.id.tv_summary, model.getSummary());
-            holder.text(R.id.tv_praise, model.getPraise() == 0 ? "点赞" : String.valueOf(model.getPraise()));
-            holder.text(R.id.tv_comment, model.getComment() == 0 ? "评论" : String.valueOf(model.getComment()));
-            holder.text(R.id.tv_read, "阅读量 " + model.getRead());
-
-            RadiusImageView imageView = holder.findViewById(R.id.iv_image);
-            MediaLoader.getInstance().getImageLoader().displayImage(imageView.getContext(), model.getImageUrl(), imageView);
-
-        } else {
-            if (mIsAnim) {
-                broccoli.addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.iv_avatar)))
-                        .addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.tv_user_name)))
-                        .addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.tv_tag)))
-                        .addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.tv_title)))
-                        .addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.tv_summary)))
-                        .addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.iv_image)))
-                        .addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.iv_praise)))
-                        .addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.iv_comment)))
-                        .addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.tv_praise)))
-                        .addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.tv_comment)))
-                        .addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.tv_read)));
-            } else {
-                broccoli.addPlaceholders(
-                        holder.findView(R.id.iv_avatar),
-                        holder.findView(R.id.tv_user_name),
-                        holder.findView(R.id.tv_tag),
-                        holder.findView(R.id.tv_title),
-                        holder.findView(R.id.tv_summary),
-                        holder.findView(R.id.iv_image),
-                        holder.findView(R.id.iv_praise),
-                        holder.findView(R.id.tv_praise),
-                        holder.findView(R.id.iv_comment),
-                        holder.findView(R.id.tv_comment),
-                        holder.findView(R.id.tv_read)
-                );
-            }
-            broccoli.show();
-        }
-
-    }
-
-    @Override
-    public SmartRecyclerAdapter<NewInfo> refresh(Collection<NewInfo> collection) {
-        mHasLoad = true;
-        return super.refresh(collection);
+        RadiusImageView imageView = holder.findViewById(R.id.iv_image);
+        MediaLoader.getInstance().getImageLoader().displayImage(imageView.getContext(), model.getImageUrl(), imageView);
     }
 
     /**
-     * 资源释放，防止内存泄漏
+     * 绑定占位控件
+     *
+     * @param holder
+     * @param broccoli
      */
-    public void recycle() {
-        for (Broccoli broccoli : mBroccoliMap.values()) {
-            broccoli.removeAllPlaceholders();
+    @Override
+    protected void onBindBroccoli(SmartViewHolder holder, Broccoli broccoli) {
+        if (mIsAnim) {
+            broccoli.addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.iv_avatar)))
+                    .addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.tv_user_name)))
+                    .addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.tv_tag)))
+                    .addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.tv_title)))
+                    .addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.tv_summary)))
+                    .addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.iv_image)))
+                    .addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.iv_praise)))
+                    .addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.iv_comment)))
+                    .addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.tv_praise)))
+                    .addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.tv_comment)))
+                    .addPlaceholder(PlaceholderHelper.getParameter(holder.findView(R.id.tv_read)));
+        } else {
+            broccoli.addPlaceholders(
+                    holder.findView(R.id.iv_avatar),
+                    holder.findView(R.id.tv_user_name),
+                    holder.findView(R.id.tv_tag),
+                    holder.findView(R.id.tv_title),
+                    holder.findView(R.id.tv_summary),
+                    holder.findView(R.id.iv_image),
+                    holder.findView(R.id.iv_praise),
+                    holder.findView(R.id.tv_praise),
+                    holder.findView(R.id.iv_comment),
+                    holder.findView(R.id.tv_comment),
+                    holder.findView(R.id.tv_read)
+            );
         }
-        mBroccoliMap.clear();
-        clear();
     }
+
 }
---
+++ app/src/main/java/com/xuexiang/xuidemo/adapter/base/BroccoliRecyclerAdapter.java
@@ -0,0 +1,90 @@
+package com.xuexiang.xuidemo.adapter.base;
+
+import android.support.annotation.LayoutRes;
+import android.view.View;
+
+import com.scwang.smartrefresh.layout.adapter.SmartRecyclerAdapter;
+import com.scwang.smartrefresh.layout.adapter.SmartViewHolder;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import me.samlss.broccoli.Broccoli;
+
+/**
+ * 使用Broccoli占位的基础适配器
+ *
+ * @author XUE
+ * @since 2019/4/8 16:33
+ */
+public abstract class BroccoliRecyclerAdapter<T> extends SmartRecyclerAdapter<T> {
+    /**
+     * 是否已经加载成功
+     */
+    private boolean mHasLoad = false;
+    private Map<View, Broccoli> mBroccoliMap = new HashMap<>();
+
+    public BroccoliRecyclerAdapter(Collection<T> collection, @LayoutRes int layoutId) {
+        super(collection, layoutId);
+    }
+
+    /**
+     * 绑定布局控件
+     *
+     * @param holder
+     * @param model
+     * @param position
+     */
+    @Override
+    protected void onBindViewHolder(SmartViewHolder holder, T model, int position) {
+        Broccoli broccoli = mBroccoliMap.get(holder.itemView);
+        if (broccoli == null) {
+            broccoli = new Broccoli();
+            mBroccoliMap.put(holder.itemView, broccoli);
+        }
+        if (mHasLoad) {
+            broccoli.removeAllPlaceholders();
+
+            onBindData(holder, model, position);
+        } else {
+            onBindBroccoli(holder, broccoli);
+            broccoli.show();
+        }
+    }
+
+    /**
+     * 绑定控件
+     *
+     * @param holder
+     * @param model
+     * @param position
+     */
+    protected abstract void onBindData(SmartViewHolder holder, T model, int position);
+
+    /**
+     * 绑定占位控件
+     *
+     * @param broccoli
+     */
+    protected abstract void onBindBroccoli(SmartViewHolder holder, Broccoli broccoli);
+
+    @Override
+    public SmartRecyclerAdapter<T> refresh(Collection<T> collection) {
+        mHasLoad = true;
+        return super.refresh(collection);
+    }
+
+    /**
+     * 资源释放，防止内存泄漏
+     */
+    public void recycle() {
+        for (Broccoli broccoli : mBroccoliMap.values()) {
+            broccoli.removeAllPlaceholders();
+        }
+        mBroccoliMap.clear();
+        clear();
+    }
+
+
+}
