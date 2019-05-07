3e04e0d7af1a178ef32c3ac94a5b0081b5df4d7c
xuexiangjys <xuexiangjys@163.com>
2019-05-08 00:19:22 +0800

    添加Material Design组件的使用案例

---
+++ app/src/main/java/com/xuexiang/xuidemo/fragment/expands/MaterialDesignFragment.java
@@ -0,0 +1,25 @@
+package com.xuexiang.xuidemo.fragment.expands;
+
+import com.xuexiang.xpage.annotation.Page;
+import com.xuexiang.xuidemo.R;
+import com.xuexiang.xuidemo.base.ComponentContainerFragment;
+import com.xuexiang.xuidemo.fragment.expands.materialdesign.ToolBarFragment;
+
+/**
+ * @author xuexiang
+ * @since 2019-05-07 23:30
+ */
+@Page(name = "Material Design", extra = R.drawable.ic_expand_material_design)
+public class MaterialDesignFragment extends ComponentContainerFragment {
+    /**
+     * 获取页面的类集合[使用@Page注解进行注册的页面]
+     *
+     * @return
+     */
+    @Override
+    protected Class[] getPagesClasses() {
+        return new Class[]{
+                ToolBarFragment.class
+        };
+    }
+}
---
+++ app/src/main/java/com/xuexiang/xuidemo/fragment/expands/materialdesign/ToolBarFragment.java
@@ -0,0 +1,30 @@
+package com.xuexiang.xuidemo.fragment.expands.materialdesign;
+
+import com.xuexiang.xpage.annotation.Page;
+import com.xuexiang.xuidemo.R;
+import com.xuexiang.xuidemo.base.BaseFragment;
+
+/**
+ * @author xuexiang
+ * @since 2019-05-08 00:13
+ */
+@Page(name = "ToolBar使用")
+public class ToolBarFragment extends BaseFragment {
+    /**
+     * 布局的资源id
+     *
+     * @return
+     */
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_toolbar;
+    }
+
+    /**
+     * 初始化控件
+     */
+    @Override
+    protected void initViews() {
+
+    }
+}
