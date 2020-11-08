a1cef4b351a8a0d9fa8c2bc5421a0d3d633753ec
xuexiangjys <xuexiangjys@163.com>
2020-11-08 23:08:43 +0800

    增加腾讯x5线上调试页面

---
+++ app/src/main/java/com/xuexiang/xuidemo/fragment/expands/webview/TBSX5Fragment.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (C) 2020 xuexiangjys(xuexiangjys@163.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.xuexiang.xuidemo.fragment.expands.webview;
+
+import android.view.KeyEvent;
+
+import androidx.fragment.app.Fragment;
+
+import com.tencent.smtt.sdk.WebChromeClient;
+import com.tencent.smtt.sdk.WebSettings;
+import com.tencent.smtt.sdk.WebView;
+import com.tencent.smtt.sdk.WebViewClient;
+import com.xuexiang.xpage.annotation.Page;
+import com.xuexiang.xpage.base.XPageFragment;
+import com.xuexiang.xpage.core.PageOption;
+import com.xuexiang.xrouter.annotation.AutoWired;
+import com.xuexiang.xrouter.launcher.XRouter;
+import com.xuexiang.xuidemo.R;
+import com.xuexiang.xuidemo.base.BaseFragment;
+import com.xuexiang.xuidemo.utils.XToastUtils;
+
+import butterknife.BindView;
+
+/**
+ * @author xuexiang
+ * @since 2020/11/8 10:34 PM
+ */
+@Page(name = "腾讯X5浏览器")
+public class TBSX5Fragment extends BaseFragment {
+
+    public static final String KEY_URL = "com.xuexiang.xuidemo.fragment.expands.webview.key_url";
+
+    /**
+     * 打开网页
+     *
+     * @param fragment 页面
+     * @param url      打开的网页
+     */
+    public static Fragment openUrl(XPageFragment fragment, String url) {
+        return PageOption.to(TBSX5Fragment.class)
+                .setNewActivity(true)
+                .putString(KEY_URL, url)
+                .open(fragment);
+    }
+
+    @BindView(R.id.webView)
+    WebView webView;
+
+    @AutoWired(name = KEY_URL)
+    String url;
+
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_tbs_x5;
+    }
+
+    @Override
+    protected void initArgs() {
+        XRouter.getInstance().inject(this);
+    }
+
+    @Override
+    protected void initViews() {
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onReceivedError(WebView var1, int var2, String var3, String var4) {
+                XToastUtils.error("网页加载失败");
+            }
+        });
+        //进度条
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+            }
+        });
+        webView.loadUrl(url);
+    }
+
+    @Override
+    public void onDestroy() {
+        if (webView != null) {
+            webView.destroy();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
