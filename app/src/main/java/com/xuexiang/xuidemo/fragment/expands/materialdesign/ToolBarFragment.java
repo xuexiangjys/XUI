ded1c6757b21fd03373723b70e365ecf626ef501
xuexiangjys <xuexiangjys@163.com>
2021-10-18 21:00:37 +0800

    基础主题修改：Theme.AppCompat.Light.NoActionBar -> Theme.MaterialComponents.Light.NoActionBar

--- app/src/main/java/com/xuexiang/xuidemo/fragment/expands/materialdesign/ToolBarFragment.java
+++ app/src/main/java/com/xuexiang/xuidemo/fragment/expands/materialdesign/ToolBarFragment.java
@@ -2,15 +2,23 @@ package com.xuexiang.xuidemo.fragment.expands.materialdesign;
 
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
 
 import com.xuexiang.xpage.annotation.Page;
+import com.xuexiang.xui.utils.ResUtils;
+import com.xuexiang.xui.utils.ViewUtils;
+import com.xuexiang.xui.widget.popupwindow.popup.XUISimplePopup;
 import com.xuexiang.xuidemo.R;
 import com.xuexiang.xuidemo.base.BaseFragment;
 import com.xuexiang.xuidemo.utils.XToastUtils;
+import com.xuexiang.xutil.common.StringUtils;
+import com.xuexiang.xutil.display.DensityUtils;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * @author xuexiang
@@ -28,6 +36,15 @@ public class ToolBarFragment extends BaseFragment {
     EditText editSearch;
     @BindView(R.id.tool_bar_4)
     Toolbar toolBar4;
+    @BindView(R.id.ll_action)
+    LinearLayout llAction;
+    @BindView(R.id.tool_bar_5)
+    Toolbar toolBar5;
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.tv_sub_title)
+    TextView tvSubTitle;
+
 
     /**
      * 布局的资源id
@@ -48,9 +65,10 @@ public class ToolBarFragment extends BaseFragment {
         initToolbar2();
         initToolbar3();
         initToolbar4();
+        initToolbar5();
     }
 
-    private void initToolbar1(){
+    private void initToolbar1() {
         //设置NavigationIcon
         toolBar1.setNavigationIcon(R.drawable.ic_navigation_menu);
         // 设置 NavigationIcon 点击事件
@@ -76,36 +94,55 @@ public class ToolBarFragment extends BaseFragment {
         toolBar1.setOnMenuItemClickListener(menuItemClickListener);
     }
 
-    private void initToolbar2(){
+    private void initToolbar2() {
         toolBar2.setNavigationOnClickListener(onClickListener);
         toolBar2.inflateMenu(R.menu.menu_setting);
         toolBar2.setOnMenuItemClickListener(menuItemClickListener);
 
     }
 
-    private void initToolbar3(){
+    private void initToolbar3() {
         toolBar3.setNavigationOnClickListener(onClickListener);
         toolBar3.inflateMenu(R.menu.menu_setting);
         toolBar3.setOnMenuItemClickListener(menuItemClickListener);
     }
 
-    private void initToolbar4(){
+    private void initToolbar4() {
         toolBar4.setNavigationOnClickListener(onClickListener);
         toolBar4.inflateMenu(R.menu.menu_search);
         toolBar4.setOnMenuItemClickListener(menuItemClickListener);
     }
 
+    private void initToolbar5() {
+        toolBar5.setNavigationOnClickListener(onClickListener);
+        toolBar5.setOnMenuItemClickListener(menuItemClickListener);
+        tvTitle.setText("主页");
+    }
+
+
     private View.OnClickListener onClickListener = v -> XToastUtils.toast("点击了NavigationIcon");
 
     Toolbar.OnMenuItemClickListener menuItemClickListener = item -> {
         XToastUtils.toast("点击了:" + item.getTitle());
-        switch (item.getItemId()){
-            case R.id.item_setting:
-                //点击设置
-                break;
-            default:
-                break;
+        if (item.getItemId() == R.id.item_setting) {
+            //点击设置
         }
         return false;
     };
+
+    @OnClick(R.id.ll_action)
+    public void onViewClicked(View view) {
+        if (view.getId() == R.id.ll_action) {
+            showSelectPopWindow(view);
+        }
+    }
+
+    private void showSelectPopWindow(View view) {
+        new XUISimplePopup<>(getContext(), ResUtils.getStringArray(R.array.grid_titles_entry))
+                .create(DensityUtils.dip2px(getContext(), 170), (adapter, item, position) -> {
+                    ViewUtils.setText(tvSubTitle, String.format("<%s>", item));
+                })
+                .setHasDivider(true)
+                .showDown(view);
+    }
 }
